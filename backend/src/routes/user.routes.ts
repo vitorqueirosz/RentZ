65aac96f950a2297c6765a2a03ebcb4165cfe8b2
Vitor Queiroz <vitor.queiroszti@gmail.com>
2020-11-08 13:25:10 -0300

    adding nodemailer and assets

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import multer from 'multer';
+import nodemailer, { createTransport } from 'nodemailer';
 import UserController from '../controller/UserController';
 import ensureAuhenticated from '../middlewares/ensureAuthenticated';
 
@@ -20,6 +21,26 @@ userRoutes.use(ensureAuhenticated);
 
 userRoutes.get('/', userController.index);
 userRoutes.get('/rents', userController.search);
+
+userRoutes.get('/email', (req, res) => {
+    const transporter = createTransport({
+        host: 'smtp.mailtrap.io',
+        port: 2525,
+        auth: {
+            user: '8e4adb5b8e0ff7',
+            pass: 'e705563bb63fae',
+        },
+    });
+
+    transporter.sendMail({
+        from: 'vitorqueiroszti@gmail.com',
+        to: 'vitorqueiroszti@gmail.com',
+        subject: 'Confirmacao de reserva - RentZ',
+        html: '<h1>Oi<h1>',
+    });
+
+    return res.json('ok');
+});
 
 userRoutes.patch(
     '/avatar',
